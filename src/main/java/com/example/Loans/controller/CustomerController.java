448484bf00979c51db6c4a68a3417015669bb0cf
FernandesLr <leo1ramosf@gmail.com>
2024-09-24 19:16:48 -0300

    Ajuste no response do controller

--- src/main/java/com/example/Loans/controller/CustomerController.java
+++ src/main/java/com/example/Loans/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.example.Loans.controller;
 
 import com.example.Loans.dto.CustomerRequestDTO;
+import com.example.Loans.service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,15 +9,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/customer-loans")
 public class CustomerController {
 
-
+    @Autowired
+    LoanService loan = new LoanService();
 
     @PostMapping
-    public ResponseEntity<String> validateLoan(@RequestBody CustomerRequestDTO request){
-        System.out.println(request);
-        return ResponseEntity.ok("ok");
+    public ResponseEntity<Map<String, Object>> validateLoan(@RequestBody CustomerRequestDTO request){
+        var loanList = loan.validateLoanRequest(request);
+
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("customer", request.getName());
+        response.put("loans", loanList);
+
+
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/example/Loans/service/LoanService.java
+++ src/main/java/com/example/Loans/service/LoanService.java
@@ -1,12 +1,30 @@
 package com.example.Loans.service;
 
+import com.example.Loans.dto.CustomerRequestDTO;
+import com.example.Loans.dto.LoanResponseDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LoanService {
 
-    public void validateLoanRequest(float salary, int age, String locale){
+    public List<LoanResponseDTO> validateLoanRequest(CustomerRequestDTO client){
+        List<LoanResponseDTO> loanList = new ArrayList<>();
 
 
+        if(client.getIncome() <= 3000){
+            loanList.add(new LoanResponseDTO("PERSONAL", 4));
+            loanList.add(new LoanResponseDTO("GUARANTEED", 3));
+        }if(client.getIncome() <=5000 && client.getAge() < 30 && client.getLocation() == "SP"){
+            loanList.add(new LoanResponseDTO("PERSONAL", 4));
+            loanList.add(new LoanResponseDTO("GUARANTEED", 3));
+        }if(client.getIncome() >= 5000){
+            loanList.add(new LoanResponseDTO("CONSIGNMENT", 2));
+        }
+        System.out.println(loanList.get(0));
+
+        return loanList;
     }
 }
